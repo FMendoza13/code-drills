dcfa653e20803038fbaede4866119d1f98adb59d
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-11-18 17:40:17 -0800

    Mostly finished, still need to figure out last conditional

---
+++ 03-javascript/code-drills/03-core-js-user-input-and-dom-1/challenge-prompt.js
@@ -0,0 +1,37 @@
+// User Input and Writing to DOM 1
+
+
+
+// Declare a variable `userName`
+var userName 
+
+// Prompt the user for their name and store their response into `userName`
+// hint: use javascript's built in `prompt` method
+
+userName = prompt("Please enter your name");
+console.log(userName);
+
+// Create an alert welcoming the user to the "Sandwich Shop" using the name they just gave us
+alert(userName + " welcome to the sandwich shop")
+
+
+// Using the `confirm` method, ask the user if they would like a sandwich
+// Store their response into a new `wantsSandwich` variable
+// window.confirm("Would you like a sandwich?")
+var wantsSandwich = window.confirm("Would you like a sandwich?");
+
+
+
+
+// Check if the user wanted a sandwich
+
+
+    // If the user did want a sandwich, use the `document.write` method to write "Your sandwich will be ready soon!" to the DOM
+if (true)
+document.write("Your sandwich will be ready soon!")
+    
+// Else, say goodbye to the user using the name they gave us earlier
+else if
+(document.write ("Goodbye " + userName));
+
+
